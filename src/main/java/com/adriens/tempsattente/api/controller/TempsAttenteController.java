a51dbb9f8ba35bd1797a99a1d9b9fa9cc00d9719
Meilie <bossart.emilie@gmail.com>
2020-03-23 13:28:54 +1100

    Lorsque l'on passe une Commune inexistanteon récupère une erreur 500 #1 On renvoie une erreur 404 lorsque la commune est inexistante (suppression d'imports inutiles, modification message d'erreur getAgence(idAgence))

--- src/main/java/com/adriens/tempsattente/api/controller/TempsAttenteController.java
+++ src/main/java/com/adriens/tempsattente/api/controller/TempsAttenteController.java
@@ -7,21 +7,19 @@ package com.adriens.tempsattente.api.controller;
 
 import com.adriens.tempsattente.api.service.TempsAttenteService;
 import com.adriens.tempsattente.exception.AgenceNotFoundException;
+import com.adriens.tempsattente.exception.CommuneNotFoundException;
 
 import com.github.adriens.opt.tempsattente.sdk.Agence;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.view.RedirectView;
 
 /**
@@ -57,14 +55,10 @@ public class TempsAttenteController {
     @GetMapping("/temps-attente/agences/{communeName}")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ArrayList<Agence> getAgences(@PathVariable String communeName) throws Exception {
-        try{
-            
-            return tempsAttenteService.getAgences(StringUtils.stripAccents(communeName.toLowerCase()));
-        }
-        catch(Exception ex){
-            log.error("");
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aucune agence pour "+communeName);
-            
+        if(tempsAttenteService.getAgences(communeName).isEmpty()) {
+            throw new CommuneNotFoundException("la commune <" +communeName+ "> n'existe pas.\n");
+        } else { 
+            return tempsAttenteService.getAgences(communeName);
         }
     }
 
@@ -83,7 +77,7 @@ public class TempsAttenteController {
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     public Agence getAgence(@PathVariable int idAgence) throws Exception {
         if(tempsAttenteService.getAgence(idAgence) == null) {
-            throw new AgenceNotFoundException("l'ID d'agence : " +idAgence+ " n'existe pas.\n");
+            throw new AgenceNotFoundException("l'ID d'agence <" +idAgence+ "> n'existe pas.\n");
         } else { 
             return tempsAttenteService.getAgence(idAgence);
         }
