ba533dc8653865cd2922f288f7c3cffc2dfa0b73
Meilie <bossart.emilie@gmail.com>
2020-03-24 09:39:56 +1100

    Implémenter TU sur chaque endpoint #7 - "/" - "/communes" - "/temps-attente/agences" - "/temps-attente/agences/{communeName}" GoodCommuneName|BadCommuneName - "/temps-attente/agence/{idAgence}" GoodID|BadID|BadIDBadRequest

--- src/test/java/com/adriens/tempsattente/api/controller/TempsAttenteControllerTest.java
+++ src/test/java/com/adriens/tempsattente/api/controller/TempsAttenteControllerTest.java
@@ -5,18 +5,24 @@
  */
 package com.adriens.tempsattente.api.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.adriens.tempsattente.api.service.TempsAttenteService;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
+import com.github.adriens.opt.tempsattente.sdk.Agence;
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  *
@@ -24,125 +30,134 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
  */
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = {TempsAttenteController.class, TempsAttenteService.class})
+@WebMvcTest
 public class TempsAttenteControllerTest {
     
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mvc;
+    
+    @Autowired
+    private ObjectMapper mapper;
     
     @Test
-    public void testRedirect() {
+    public void testRedirect() throws Exception {
         System.out.println("redirect");
         
-        String url = "http://localhost:8080/";
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        MvcResult result = (MvcResult) mvc.perform( MockMvcRequestBuilders
+        .get("/")
+        .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isFound())
+        .andReturn();
         
-        assertEquals(HttpStatus.FOUND, response.getStatusCode());
+        assertEquals("https://github.com/adriens/opt-temps-attente-agences-api", result.getResponse().getRedirectedUrl());
     }
 
     @Test
     public void testGetAgences_0args() throws Exception {
         System.out.println("getAgencesAll");
         
-        String url = "http://localhost:8080/temps-attente/agences";
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        MvcResult result = (MvcResult) mvc.perform( MockMvcRequestBuilders
+        .get("/temps-attente/agences")
+        .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
         
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(response.getBody());
+        ArrayList<Agence> listeAgence = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<ArrayList<Agence>>(){});
         
-        int i = 0;
-        for (JsonNode jsonNode : root) {
-            i++;
-        }
-        
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(65,i);
+        assertEquals(65, listeAgence.size());
     }
-     
+    
     @Test
     public void testGetAgencesGood_String() throws Exception {
         System.out.println("getAgencesGoodCommuneName");
         
-        String communeName = "noumea";
-        String url = "http://localhost:8080/temps-attente/agences/";
-        ResponseEntity<String> response = restTemplate.getForEntity(url + communeName, String.class);
+        MvcResult result = (MvcResult) mvc.perform( MockMvcRequestBuilders
+        .get("/temps-attente/agences/{communeName}","noumea")
+        .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
         
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(response.getBody());
+        ArrayList<Agence> listeAgence = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<ArrayList<Agence>>(){});
         
-        int i = 0;
-        for (JsonNode jsonNode : root) {
-            i++;
-        }
-        
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(17, i);
+        assertEquals(17, listeAgence.size());
     }
     
     @Test
     public void testGetAgencesBad_String() throws Exception {
         System.out.println("getAgencesBadCommuneName");
         
-        String communeName = "noum";
-        String url = "http://localhost:8080/temps-attente/agences/";
-        ResponseEntity<String> response = restTemplate.getForEntity(url + communeName, String.class);
+        MvcResult result = (MvcResult) mvc.perform( MockMvcRequestBuilders
+        .get("/temps-attente/agences/{communeName}","noum")
+        .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isNotFound())
+        .andReturn();
         
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertTrue(result.getResponse().getContentAsString().isEmpty());
     }
 
     @Test
     public void testGetCommunesNames() throws Exception {
         System.out.println("getCommunesNames");
         
-        String url = "http://localhost:8080/communes";
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        MvcResult result = (MvcResult) mvc.perform( MockMvcRequestBuilders
+        .get("/communes")
+        .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
         
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(response.getBody());
+        ArrayList<String> listeCommunes = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<ArrayList<String>>(){});
         
-        int i = 0;
-        for (JsonNode jsonNode : root) {
-            i++;
-        }
-        
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(33, i);
+        assertEquals(33, listeCommunes.size());
     }
 
     @Test
     public void testGetAgenceGoodID() throws Exception {
         System.out.println("getAgenceGoodID");
+       
+        MvcResult result = (MvcResult) mvc.perform( MockMvcRequestBuilders
+        .get("/temps-attente/agence/{idAgence}",4306)
+        .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
         
-        int idAgence = 4177;
-        String url = "http://localhost:8080/temps-attente/agence/";
-        ResponseEntity<String> response = restTemplate.getForEntity(url + idAgence, String.class);
+        Agence agence = mapper.readValue(result.getResponse().getContentAsString(), Agence.class);
+        String strAgence = "Agence 4306 <designation: Agence de LA FOA, realMaxWaitingTimeMs: 0, coordonneeX: 382333.56809999794, coordonneeY: 276615.18469999917, coordonneeXPrecise: 382334, coordonneeYPrecise: 276617>";
         
-        ObjectMapper mapper = new ObjectMapper();
-        String agence = "{\"idAgence\":4177,\"designation\":\"Agence philatélique CALEDOSCOPE\",\"realMaxWaitingTimeMs\":0,\"coordonneeX\":445205.7199999988,\"coordonneeY\":214495.6400000006,\"coordonneeXPrecise\":445227,\"coordonneeYPrecise\":214500}";
-        
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(agence, response.getBody());
+        assertEquals(strAgence, agence.toString());
     }
     
     @Test
     public void testGetAgenceBadIDNotFound() throws Exception {
         System.out.println("getAgenceBadIDNotFound");
         
-        int idAgence = 2;
-        String url = "http://localhost:8080/temps-attente/agence/";
-        ResponseEntity<String> response = restTemplate.getForEntity(url + idAgence, String.class);
+        MvcResult result = (MvcResult) mvc.perform( MockMvcRequestBuilders
+        .get("/temps-attente/agence/{idAgence}",2)
+        .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isNotFound())
+        .andReturn();
         
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertTrue(result.getResponse().getContentAsString().isEmpty());
     }
     
     @Test
     public void testGetAgenceBadIDBadRequest() throws Exception {
         System.out.println("getAgenceBadIDBadRequest");
         
-        String idAgence = "toto";
-        String url = "http://localhost:8080/temps-attente/agence/";
-        ResponseEntity<String> response = restTemplate.getForEntity(url + idAgence, String.class);
+        MvcResult result = (MvcResult) mvc.perform( MockMvcRequestBuilders
+        .get("/temps-attente/agence/{idAgence}","toto")
+        .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isBadRequest())
+        .andReturn();
         
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertTrue(result.getResponse().getContentAsString().isEmpty());
     }
 }
